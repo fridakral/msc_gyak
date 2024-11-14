0c050d95a3f46015e960f946e7f386de5c54e136
bidamida <kralfrida@gmail.com>
2024-11-14 11:23:58 +0100

    hozzáadva dolgok

--- MscGyak/MscGyak/Program.cs
+++ MscGyak/MscGyak/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+using System.Collections;
+
 Console.WriteLine("Hello, World!");
+
+//Kipróbáltam a Baleset osztályt
+
+Baleset balesetObjektum = new Baleset();
+Baleset ujBaleset = new Baleset([232, 233]);
+Baleset baleset3 = new Baleset([232, 233], 2);
+
+Console.WriteLine(balesetObjektum.Fokozat);
+Console.WriteLine(ujBaleset.Fokozat);
+Console.WriteLine(baleset3.Fokozat);
+
+Console.WriteLine(balesetObjektum.Datum);
+
+/*
+balesetObjektum.Datum = DateTime.Now;
+balesetObjektum.Fokozat= 4;
+//Console.WriteLine(baleset.Datum + " " + baleset.Fokozat);
+String asdasdasd = "valami szöveg";
+Console.WriteLine(asdasdasd.Equals("más szöveg"));
+*/
+
---
+++ MscGyak/MscGyak/model/Baleset.cs
@@ -0,0 +1,32 @@
+
+using System;
+
+public class Baleset
+{
+    public int[] Koordinata;
+    public DateTime Datum;
+    public bool SerulesTortent;
+    public int Fokozat;
+
+    public Baleset()
+    {
+        this.Datum = DateTime.Now;
+    }
+
+    public Baleset(int[] koordinata)
+    {
+        Koordinata = koordinata;
+    }
+
+    public Baleset(int[] koordinata, int fokozat)
+    {
+        Koordinata = koordinata;
+        Fokozat = fokozat;
+    }
+
+    public int[] GridKalkulator()
+    {
+        return Koordinata;
+    }
+}
+
