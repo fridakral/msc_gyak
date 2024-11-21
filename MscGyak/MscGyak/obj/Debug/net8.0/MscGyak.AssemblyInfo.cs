6e99459640a527eddac171118ae3aa707e3c784f
bidamida <kralfrida@gmail.com>
2024-11-21 16:00:12 +0100

    baleset ősosztály létrehozva

--- MscGyak/MscGyak/obj/Debug/net8.0/MscGyak.AssemblyInfo.cs
+++ MscGyak/MscGyak/obj/Debug/net8.0/MscGyak.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MscGyak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c565fa9da122b60a7ecc5c266518195240b97d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ed0f29bdb832f272f43ce3e5f201c2d9c6dab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MscGyak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MscGyak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
